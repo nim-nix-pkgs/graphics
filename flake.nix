00a078e1a5e10ad4fa15baec654eb00280493cf4
hugosenari <hugosenari@gmail.com>
2022-03-12 03:17:01 -0300

    chore: re index graphics

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Graphics module for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."graphics-master".url = "path:./master";
+  inputs."graphics-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."graphics-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
